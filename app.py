fc59eb4ff498c2e45c1de22a688cf5c79a47a0bd
deva7411 <enochdeevenvm@gmail.com>
2025-07-05 18:57:32 +0530

    up3

--- app.py
+++ app.py
@@ -146,4 +146,5 @@ def to_pdf():
         return f"🔥 Error during conversion: {e}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
+    app.run(host='0.0.0.0', port=port)
